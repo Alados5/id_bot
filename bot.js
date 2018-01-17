b184b30be0f59f5307094d20dfc963b8be423e53
Alados5 <adria.alados@gmail.com>
2018-01-17 21:04:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,2 +1,36 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+var id_list = {};
+const prefix = '!';
+const ownerID = "284104569586450434";
+
+client.on('ready', () => {
+  console.log('Ready');
+  client.user.setActivity(`${prefix}help | ${client.guilds.size} servers`)
+});
+
+client.on('message', msg => {
+  //Checks if author is a bot or message doesn't start with prefix
+  if(msg.author.bot || !msg.content.startsWith(prefix)) return;
+  
+  //Checks if the author is me
+  if(msg.author.id == ownerID && msg.content == '!MyBot') {
+    msg.reply('What is thy bidding, my Master?')
+  }
+  
+  //Handles arguments to just take the first word
+  const args = msg.content.slice('!'.length).split(/ +/);
+  const command = args.shift().toLowerCase();  
+  
+  
+  
+  
+});
+
+
+
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
